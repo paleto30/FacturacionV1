a7bfe1a978123655bda986bf5a729ac31e1d28bd
paleto30 <paleto0817@gmail.com>
2023-06-08 07:43:34 -0500

    fix (cambios) : :sparkles: cambio de logica
    se modifico la logica , ya no se esta llamando un componente extra, solo se esta llamando una plantilla html en el mismo componente y lo que hacemos es incluir esa plantilla.

--- components/productsForm/productsForm.js
+++ components/productsForm/productsForm.js
@@ -6,13 +6,20 @@ export class formProductos extends HTMLElement{
         this.contador = 0;
     }
 
+    async raw(){
+        let data = await (await fetch("./components/productsForm/productoRaw.html")).text();
+        return data;
+    } 
+
     async component(){
         let data = await (await fetch("./components/productsForm/productsForm.html")).text();
         return data;
     }
 
+
     crearNodo(){
         let element =  this.querySelector("#raw_0");
+
         let clone = element.cloneNode(true);
         let idclone = clone.id.slice(0,-1);
         clone.id = idclone+`${this.contador+1}`
@@ -21,25 +28,44 @@ export class formProductos extends HTMLElement{
             element.id = newId+`${this.contador+1}`;
             element.name = newId+`${this.contador+1}`;
         });
+        clone.classList.remove("first")
+
         this.contador ++;
         return clone;
     }   
 
     add(e){
-
         let hijo = this.crearNodo();
         console.log(hijo);
-        console.log(typeof(hijo));
-        //console.log(typeof(hijo.parentNode));}
         this.querySelector("#list").appendChild(hijo);
     }
 
+    
+
+    /* 
+    facturar(e){        
+        console.log("hola");
+    }}
+    */
+
+    sumar(e){
+        let btn = document.querySelector(".btnAdd");
+        console.log(btn.parentNode.parentNode);
+    } 
+
     connectedCallback(){
-        this.component().then(res => {
+
+        this.component().then(async res => {
             this.innerHTML = res;
+            let html = await this.raw();
+            this.list = this.querySelector("#list").innerHTML = html;
+            this.list = this.querySelector("#list").insertAdjacentElement("beforeend",this.crearNodo());
             this.add =  this.querySelector("#add").addEventListener("click",this.add.bind(this));
-            
+            this.sumar = this.querySelector("#list").addEventListener("click",this.sumar.bind(this));
+            /*this.getDato = this.querySelector("#facturar").addEventListener("click",this.facturar.bind(this)); */
         }); 
+
+
     }
 }
 
